a530b1833d9b5762d8e36ed9e50abee21ddfa05d
Oliver Coldwell <olivercoldwell@gmail.com>
2021-04-18 14:01:42 +0100

    changed index.js routing

--- motoreasynode/index.js
+++ motoreasynode/index.js
@@ -1,30 +1,33 @@
-import 'dotenv/config.js';
-import express from 'express';
+import "dotenv/config.js";
+import path from "path";
+import express from "express";
 import mongoose from "mongoose";
 import routes from "./routes/routes.js";
 const PORT = process.env.PORT || 8080;
-
+const __dirname = path.resolve();
 
 mongoose.connect(process.env.CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(()=> {
-  const app = express();
-  app.use(express.static(path.resolve(__dirname, '../motoreasyfrontend/build')))
-  app.use("/api", routes)
-  app.use(express.json())
-  app.listen(PORT, () => {
-  console.log("Server has started")
-})
-}).catch(error => console.log(error));
-
-app.get('*', (req,res => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-}))
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+const app = express();
+app.use(express.static(path.resolve(__dirname, "../motoreasyfrontend/build")));
+app.use("/api", routes);
+app.use(express.json());
+app.get(
+  "*",
+  (req,res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../motoreasybackend/build", "index.html")
+    );
+  });
 const db = mongoose.connection;
-mongoose.set('bufferCommands', false);
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => {
-  console.log('connected')
-})
+mongoose.set("bufferCommands", false);
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("connected");
+});
+app.listen(PORT, () => {
+  console.log("Server has started");
+});
